1f4ec426790ff5aa38e6f71d8ee5d85a5480b383
aed7 <adavila316@gmail.com>
2014-10-22 22:01:41 -0400

    RabbitMq tutorials

---
+++ send.php
@@ -0,0 +1,29 @@
+<?php
+
+// Include the autoload library and use the necessary classes
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+// Create a connection to the server
+
+$connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+// Declare the queue
+
+$channel->queue_declare('hello', false, false, false, false);
+
+$msg = new AMQPMessage('Hello World!');
+$channel->basic_publish($msg, '', 'hello');
+
+echo " [x] Sent 'Hello World IS421 ( Alex is here)!'\n";
+
+// Close the channel and the connection
+
+$channel->close();
+$connection->close();
+?>
+
+
